96558a09d99c828d7a4026a36467c4ed78379310
Eduardo Cerqueira <egcerqueira@gmail.com>
2012-09-12 13:58:17 -0300

    alterações na nomenclatura das classes de controle para ficar no padrao de convencao do play

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,19 +5,13 @@ import play.mvc.Controller;
 public class Application extends Controller {
 
 	public static void index() {
-
 		 String user = Security.connected();
-
 	     render(user);
-
 	}
 
 	public static void sair() throws Throwable {
-
 		Secure.logout();
-
 		index();
-
 	}
 
 }
